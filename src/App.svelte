6cff25e17b77c913126a02ecb015951bbfc0a23a
Michael Saffold <me@93.contact>
2022-09-03 22:31:51 -0700

    init

---
+++ src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  import avi from './assets/halo.png'
+</script>
+<main>
+  <div class="hero min-h-screen bg-gradient-to-b from-blue-500 to-blue-300">
+    <div class="hero-overlay bg-opacity-60" />
+    <div class="hero-content text-center text-neutral-content">
+      <div class="max-w-md">
+        <div class="avatar">
+          <div
+            class="w-32 p-4 rounded-full ring ring-primary ring-offset-blue-300 ring-offset-2"
+          >
+            <img alt="me" src={avi} />
+          </div>
+        </div>
+        <h1 class="mb-2 text-5xl font-bold">Michael Saffold</h1>
+        <p class="mb-1">AS Computer Network Engineering • SBCC 2017</p>
+        <p class="mb-2">🍋 Goleta, CA</p>
+        <div class="justify-center">
+          <a
+            class="btn btn-primary my-2"
+            href="https://www.linkedin.com/in/michaelsaffold/">LinkedIn</a
+          >
+          <a
+            class="btn btn-primary my-2"
+            href="https://github.com/MichaelSaffold">Github</a
+          >
+        </div>
+      </div>
+    </div>
+  </div>
+</main>
